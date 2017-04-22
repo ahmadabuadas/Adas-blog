998b15384c5d167b75e40de5625ff7de96dd0574
ahmad <adas.2016@yahoo.com>
2017-04-22 05:06:27 +0000

    add user data to article listing page

--- db/migrate/20170413223013_add_name_to_articles.rb
+++ db/migrate/20170413223013_add_name_to_articles.rb
@@ -1,7 +1,6 @@
 class AddNameToArticles < ActiveRecord::Migration
   def change
     add_column :articles, :description, :text
-    add_column :articles, :create_at, :datetime
-    add_column :articles, :update_at, :datetime
+   
   end
 end
