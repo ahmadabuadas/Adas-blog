3f81994ea36225d8e72878730051e43937ae9aff
ahmad <adas.2016@yahoo.com>
2017-04-22 03:20:57 +0000

    fix some error for user id

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,8 +13,8 @@ class ArticlesController < ApplicationController
     end
     
     def create
-        debugger
         @article = Article.new(article_params)
+        @article.user = User.first
         if @article.save
             flash[:success] = "article was sucssess add"
             redirect_to article_path (@article)
